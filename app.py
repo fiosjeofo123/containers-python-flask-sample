e79f3de1dfe9024fe457a79d916f37f194f7a945
fiosjeofo123 <131018475+fiosjeofo123@users.noreply.github.com>
2026-01-18 14:40:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "Hello Back4apper!"
+def run():
+    while True:
+        print("HI")
+        import time as t
+        t.sleep(1)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
